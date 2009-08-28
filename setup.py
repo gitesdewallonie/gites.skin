2f96428b0206066637c5db0efc385b2357b3dd20
Jean-François Roche <jfroche@affinitic.be>
2009-08-28 15:21:39 +0000

    add generic function in gites.core

--- setup.py
+++ setup.py
@@ -25,17 +25,9 @@ setup(name='gites.skin',
       zip_safe=False,
       install_requires=[
           'setuptools',
-          'zc.table',
-          # -*- Extra requirements: -*-
+          'gites.core',
+          'zc.table'
       ],
       entry_points="""
       # -*- Entry points: -*-
-
-      [distutils.setup_keywords]
-      paster_plugins = setuptools.dist:assert_string_list
-
-      [egg_info.writers]
-      paster_plugins.txt = setuptools.command.egg_info:write_arg
-      """,
-      paster_plugins = ["ZopeSkel"],
-      )
+      """)
