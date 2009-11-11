586ebcdc21c9d1e10be57f0ca71e7470f3587a9a
Alain Meurant <alain.meurant@affinitic.be>
2009-11-11 16:17:48 +0000

    formulaire de mise à jour des infos du prorio et de l'hébergement

--- gites/skin/browser/proprio.py
+++ gites/skin/browser/proprio.py
@@ -47,3 +47,54 @@ class ProprioInfo(BrowserView):
         query = query.filter(proprioTable.pro_log == loginProprio)
         proprio = query.all()
         return proprio
+
+    def getAllCommunes(self):
+        """
+        Sélectionne les communes et les cp
+        """
+        wrapper = getSAWrapper('gites_wallons')
+        session = wrapper.session
+        communeTable = wrapper.getMapper('commune')
+        query = session.query(communeTable)
+        communes = query.all()
+        return communes
+
+    def getAllProprioMaj(self):
+        """
+        Liste tous les prorios en attente de modification
+        """
+        wrapper = getSAWrapper('gites_wallons')
+        session = wrapper.session
+        proprioMajTable = wrapper.getMapper('proprio_maj')
+        query = session.query(proprioMajTable)
+        propriosMaj = query.all()
+        return propriosMaj
+        
+    def addProprioMaj(self):
+        """
+        ajoute les infos mise à jour par proprio dans table provisoire
+        """
+        fields = self.context.REQUEST
+        wrapper = getSAWrapper('gites_wallons')
+        session = wrapper.session
+        insertProprioMaj = wrapper.getMapper('proprio_maj')
+        newEntry = insertProprioMaj(pro_maj_propk=fields.get('pro_maj_propk'),\
+                                    pro_maj_civ_fk=fields.get('pro_maj_civ_fk'),\
+                                    pro_maj_nom1=fields.get('pro_maj_nom1'),\
+                                    pro_maj_prenom1=fields.get('pro_maj_prenom1'),\
+                                    pro_maj_nom2=fields.get('pro_maj_nom2'),\
+                                    pro_maj_prenom2=fields.get('pro_maj_prenom2'),\
+                                    pro_maj_societe=fields.get('pro_maj_societe'),\
+                                    pro_maj_adresse=fields.get('pro_maj_adresse'),\
+                                    pro_maj_com_fk=fields.get('pro_maj_com_fk'),\
+                                    pro_maj_email=fields.get('pro_maj_email'),\
+                                    pro_maj_tel_priv=fields.get('pro_maj_tel_priv'),\
+                                    pro_maj_fax_priv=fields.get('pro_maj_fax_priv'),\
+                                    pro_maj_gsm1=fields.get('pro_maj_gsm1'),\
+                                    pro_maj_url=fields.get('pro_maj_url'),\
+                                    pro_maj_tva=fields.get('pro_maj_tva'),\
+                                    pro_maj_langue=fields.get('pro_maj_langue'),\
+                                    pro_maj_data=fields.get('pro_maj_data'))
+        session.save(newEntry)
+        session.flush()
+
