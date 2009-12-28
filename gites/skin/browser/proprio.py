12abf0a4c9bbd9f8f01291bcfda22ba24a8bf3d8
Alain Meurant <alain.meurant@affinitic.be>
2009-12-28 15:50:29 +0000

    maj info proprio : gestion des updates en cas de modif trop rapprochées sans validation; check de propk en arrivées; gestion des erreurs; page insertion avec message de fin opération et status

--- gites/skin/browser/proprio.py
+++ gites/skin/browser/proprio.py
@@ -44,7 +44,6 @@ class ProprioInfo(BrowserView):
         pm = getToolByName(self, 'portal_membership')
         user = pm.getAuthenticatedMember()
         proprioLogin = user.getUserName()
-
         wrapper = getSAWrapper('gites_wallons')
         session = wrapper.session
         proprioTable = wrapper.getMapper('proprio')
@@ -86,12 +85,29 @@ class ProprioInfo(BrowserView):
         propriosMaj = query.all()
         return propriosMaj
 
+    def getProprioMajByProPk(self, proPk):
+        """
+        retourne un proprio selon sa clé depuis la table proprio_maj
+        """
+        proprioMajExist = False
+        wrapper = getSAWrapper('gites_wallons')
+        session = wrapper.session
+        proprioMajTable = wrapper.getMapper('proprio_maj')
+        query = session.query(proprioMajTable)
+        query = query.filter(proprioMajTable.pro_maj_propk == proPk)
+        records = query.all()
+        if len(records) > 0:
+            proprioMajExist = True
+        else:
+            proprioMajExist = False
+        return proprioMajExist
+
     def sendMail(self, sujet, message):
         """
         envoi de mail à secretariat GDW
         """
         #mailer = Mailer("localhost", "rof@apefasbl.org")
-        mailer = Mailer("relay.skynet.be", "alain.meurant@affinitic.be")
+        mailer = Mailer("smtp.scarlet.be", "alain.meurant@affinitic.be")
         mailer.setSubject(sujet)
         mailer.setRecipients("alain.meurant@affinitic.be")
         #mailer.setRecipients("alain.meurant@skynet.be")
@@ -107,24 +123,54 @@ class ProprioInfo(BrowserView):
         updateProprio = wrapper.getMapper('proprio')
         query = session.query(updateProprio)
         query = query.filter(updateProprio.pro_pk == proPk)
-        records = query.all()
-        for record in records:
-            record.pro_maj_info_etat = proMajInfoEtat
+        record = query.one()
+        record.pro_maj_info_etat = proMajInfoEtat
+        session.save_or_update(record)
         session.flush()
 
-    def addProprioMaj(self):
+    def  updateProprioMaj(self):
         """
-        ajoute les infos mise à jour par proprio dans table provisoire
+        mise à jour des données maj du proprio
         """
-        fields = self.context.REQUEST
+        fields = self.request
+        proMajProPk=fields.get('pro_maj_propk')
+        wrapper = getSAWrapper('gites_wallons')
+        session = wrapper.session
+        updateProprioMaj = wrapper.getMapper('proprio_maj')
+        query = session.query(updateProprioMaj)
+        query = query.filter(updateProprioMaj.pro_maj_propk == proMajProPk)
+        record = query.one()
+        record.pro_maj_civ_fk = fields.get('pro_maj_civ_fk')
+        record.pro_maj_nom1 = unicode(fields.get('pro_maj_nom1'), 'utf-8')
+        record.pro_maj_prenom1 = unicode(fields.get('pro_maj_prenom1'), 'utf-8')
+        record.pro_maj_nom2 = unicode(fields.get('pro_maj_nom2'), 'utf-8')
+        record.pro_maj_prenom2 = unicode(fields.get('pro_maj_prenom2'), 'utf-8')
+        record.pro_maj_societe = unicode(fields.get('pro_maj_societe'), 'utf-8')
+        record.pro_maj_adresse = unicode(fields.get('pro_maj_adresse'), 'utf-8')
+        record.pro_maj_com_fk = fields.get('pro_maj_com_fk')
+        record.pro_maj_email = fields.get('pro_maj_email')
+        record.pro_maj_tel_priv = fields.get('pro_maj_tel_priv')
+        record.pro_maj_fax_priv = fields.get('pro_maj_fax_priv')
+        record.pro_maj_gsm1 = fields.get('pro_maj_gsm1')
+        record.pro_maj_url = fields.get('pro_maj_url')
+        record.pro_maj_tva = fields.get('pro_maj_tva')
+        record.pro_maj_langue = fields.get('pro_maj_langue')
+        record.pro_maj_info_etat = unicode(fields.get('pro_maj_info_etat'), 'utf-8')
+        session.add(record)
+        session.flush()
+
+    def insertProprioMaj(self):
+        """
+        insère les infos mise à jour par proprio dans table provisoire
+        """
+        fields = self.request
         proPk=fields.get('pro_maj_propk')
-        proNom=fields.get('pro_maj_nom1')
         wrapper = getSAWrapper('gites_wallons')
         session = wrapper.session
         insertProprioMaj = wrapper.getMapper('proprio_maj')
         newEntry = insertProprioMaj(pro_maj_propk=proPk,\
                                     pro_maj_civ_fk=fields.get('pro_maj_civ_fk'),\
-                                    pro_maj_nom1=proNom,\
+                                    pro_maj_nom1=fields.get('pro_maj_pronom1'),\
                                     pro_maj_prenom1=fields.get('pro_maj_prenom1'),\
                                     pro_maj_nom2=fields.get('pro_maj_nom2'),\
                                     pro_maj_prenom2=fields.get('pro_maj_prenom2'),\
@@ -139,14 +185,40 @@ class ProprioInfo(BrowserView):
                                     pro_maj_tva=fields.get('pro_maj_tva'),\
                                     pro_maj_langue=fields.get('pro_maj_langue'),\
                                     pro_maj_info_etat=fields.get('pro_maj_info_etat'))
-        session.save(newEntry)
+        session.add(newEntry)
         session.flush()
 
-        proMajInfoEtat="En attente confirmation"
-        self.modifyStatutMajProprio(proPk, proMajInfoEtat)
+    def addProprioMaj(self):
+        """
+        gestion de l'ajout des données de maj par le proprio
+        """
+        fields = self.request
+        proPk = fields.get('pro_maj_propk')
+        proprio = self.getProprioByLogin()
+        for elem in proprio:
+            propriopk=elem.pro_pk
+        
+        if int(proPk) == propriopk:
+            proNom = fields.get('pro_maj_pronom1')
+            isProprioMajExist = self.getProprioMajByProPk(proPk)
 
-        sujet="Un proprio a modifié ses données personnelles"
-        message="""Le proprio %s dont la référence est %s vient de modifier ses
-                   données. Il faut les vérifier et les valider
-                   via le lien suivant"""%(proNom, proPk)
-        self.sendMail(sujet, message)
+            if isProprioMajExist:
+                self.updateProprioMaj()
+            else:
+                self.insertProprioMaj()
+
+            proMajInfoEtat="En attente confirmation"
+            self.modifyStatutMajProprio(proPk, proMajInfoEtat)
+
+            sujet="Un proprio a modifié ses données personnelles"
+            message="""Le proprio %s dont la référence est %s vient de modifier ses
+                       données. Il faut les vérifier et les valider
+                       via le lien suivant"""%(proNom, proPk)
+            self.sendMail(sujet, message)
+            return {'status':1}
+        else:
+            sujet="Un proprio a essayé modifié ses données personnelles"
+            message="""Le proprio %s dont la référence est %s a essayé de modifier ses
+                       données. Le processus est avorté suite à un problème de PK"""%(proNom, proPk)
+            self.sendMail(sujet, message)
+            return {'status':-1}
