4559946043ccbbf8ef82d9fbe2ca567ec0df107b
Alain Meurant <alain.meurant@affinitic.be>
2010-01-14 09:04:45 +0000

    big charge_fk ##

--- gites/skin/browser/hebergement.py
+++ gites/skin/browser/hebergement.py
@@ -489,7 +489,7 @@ class HebergementInfo(BrowserView):
                                         heb_maj_tarif_chmbr_autre_1p=fields.get('heb_maj_tarif_chmbr_autre_1p'),\
                                         heb_maj_tarif_chmbr_autre_2p=fields.get('heb_maj_tarif_chmbr_autre_2p'),\
                                         heb_maj_tarif_chmbr_autre_3p=fields.get('heb_maj_tarif_chmbr_autre_3p'),\
-                                        heb_maj_charge_fk=int(chargeFk))
+                                        heb_maj_charge_fk=chargeFk)
         session.save(newEntry)
         session.flush()
 
