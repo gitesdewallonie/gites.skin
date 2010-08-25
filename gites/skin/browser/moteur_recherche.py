a140a7543fac0a50ce8681a3df28001d1e3bae1e
Laurent Lasudry <lasudry@gmail.com>
2010-08-25 09:52:35 +0000

    Add name/pk search feature Change translations and fix html entities This refs #2175

--- gites/skin/browser/moteur_recherche.py
+++ gites/skin/browser/moteur_recherche.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from Products.Five import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFCore.utils import getToolByName
 from z3c.sqlalchemy import getSAWrapper
 from zope.component import queryMultiAdapter
@@ -8,30 +9,47 @@ from zope.component import queryMultiAdapter
 
 class MoteurRecherche(BrowserView):
 
-    def getHebergementByPk(self, heb_pk):
+    search_results = ViewPageTemplateFile('templates/search_results_hebergement.pt')
+
+    def getHebergementByNameOrPk(self, reference):
         """
-        Get the url of the hebergement by Pk
+        Get the url of the hebergement by Pk or part of the name
         """
         wrapper = getSAWrapper('gites_wallons')
         session = wrapper.session
-        HebTable = wrapper.getMapper('hebergement')
+        hebTable = wrapper.getMapper('hebergement')
+        proprioTable = wrapper.getMapper('proprio')
         try:
-            int(heb_pk)
+            int(reference)
         except ValueError:
-            portal = getToolByName(self.context, 'portal_url').getPortalObject()
-            return queryMultiAdapter((portal, self.request),
-                                      name="unknown_gites")()
-        hebergement = session.query(HebTable).get(heb_pk)
-        if hebergement and \
-           int(hebergement.heb_site_public) == 1 and \
-           hebergement.proprio.pro_etat:
-           # L'hébergement doit être actif, ainsi que son propriétaire
-            hebURL = queryMultiAdapter((hebergement.__of__(self.context.hebergement), self.request), name="url")
-            return self.request.RESPONSE.redirect(str(hebURL))
+            # we have a heb name to search for
+            query = session.query(hebTable).join('proprio')
+            query = query.filter(hebTable.heb_site_public == '1')
+            query = query.filter(proprioTable.pro_etat == True)
+            query = query.filter(hebTable.heb_nom.ilike("%%%s%%" % reference))
+            query = query.order_by(hebTable.heb_nom)
+            query = query.limit(100)  # performance matters
+            self.selectedHebergements = [hebergement.__of__(self.context.hebergement) for hebergement in query.all()]
+            if len(self.selectedHebergements) == 1:
+                hebURL = queryMultiAdapter((self.selectedHebergements[0], self.request),
+                                           name="url")
+                self.request.response.redirect(str(hebURL))
+                return ''
+            return self.search_results()
         else:
-            portal = getToolByName(self.context, 'portal_url').getPortalObject()
-            return queryMultiAdapter((portal, self.request),
-                                       name="unknown_gites")()
+            # we have a heb pk to search for
+            hebergement = session.query(hebTable).get(reference)
+            if hebergement and \
+               int(hebergement.heb_site_public) == 1 and \
+               hebergement.proprio.pro_etat:
+               # L'hébergement doit être actif, ainsi que son propriétaire
+                hebURL = queryMultiAdapter((hebergement.__of__(self.context.hebergement), self.request), name="url")
+                self.request.response.redirect(str(hebURL))
+                return ''
+            else:
+                portal = getToolByName(self.context, 'portal_url').getPortalObject()
+                return queryMultiAdapter((portal, self.request),
+                                           name="unknown_gites")()
 
     def getGroupedHebergementTypes(self):
         """
